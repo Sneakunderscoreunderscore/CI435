812ba4ad356053c0fd7c89ff4cc3a5363ebbc310
Sneakunderscoreunderscore <rhionryan@gmail.com>
2024-03-14 15:27:21 +0000

    semester 2 week 2

---
+++ js/s2script.js
@@ -0,0 +1,16 @@
+console.log("Hello world!");
+
+var a = 5;
+console.log(a);
+var b = 3;
+console.log(b);
+
+console.log(a*b);
+console.log((a+b)%3);
+
+var s1 = "Hello";
+var s2 = "User";
+console.log(s1+" "+s2)
+
+var squared = a*a;
+console.log(a+" squared is "+squared)
